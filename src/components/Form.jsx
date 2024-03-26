d72ee9ffe174d237eb55ea8ce37f167647f0f1dd
CorinaTalledo <corina.talledo@gmail.com>
2024-03-25 22:13:05 -0300

    Installed and set of Formik with 4 validations + Replace hook useState with useFormik ()

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,38 +3,40 @@ import IconButton from '@mui/material/IconButton';
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
 import FormControl from '@mui/material/FormControl';
 
-import { useState } from 'react';
+import { useFormik } from 'formik';  
 
-export default function Form({ addTask }) {
+export default function Form({ addTask, tasks }) {
 
-  // // Estado para guardar la nueva tarea que va a agregarse al listado general
-  const [newTask, setNewTask] = useState({
-    taskDescription: "",
-    state: false,
-    id: crypto.randomUUID()
-  })
-
-
-    // Actualiza el estado de la task, o sea las letritas  
-    const handleInput = (e) =>{
-      setNewTask({...newTask, taskDescription: e.target.value})
-    }
-
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-    addTask(newTask)
-
-    setNewTask({
+  const formik = useFormik({
+    initialValues:{
       taskDescription: "",
       state: false,
       id: crypto.randomUUID()
-    })
-  }
+    },
+    validate: (values) =>{
+      const errors = {};
+        if (!values.taskDescription) {
+          errors.taskDescription = 'Escribe una tarea';
+        } else if (values.taskDescription.length > 30) {
+          errors.taskDescription = 'No puedes ingresar tareas de más de 30 caracteres';
+        } else if (values.taskDescription.length < 5) {
+          errors.taskDescription = 'Por favor, ingresa una tarea que contenga mas de 5 caracteres';
+        } else if (tasks.includes(values.taskDescription))
+          errors.taskDescription = 'Ya has ingresado esta tarea, ingresa otra o destildá la existente';
+      return errors;
+    },
+    onSubmit: (values) =>{
+      addTask(values)
+    }
+  });
 
 
   return (
-      <FormControl component="form" sx={{ width: '50%' }} noValidate autoComplete="off" onSubmit={handleSubmit} >
-        <TextField id="filled-basic" label="Insert Task" variant="filled" sx={{ width: '80%' }} onChange={handleInput} value={newTask.taskDescription} />
+      <FormControl component="form" sx={{ width: '50%' }} noValidate autoComplete="off" onSubmit={formik.handleSubmit} >
+        <TextField id="taskDescription" name='taskDescription' label="Insert Task" variant="filled" sx={{ width: '80%' }} onChange={formik.handleChange} value={formik.values.taskDescription} onBlur={formik.handleBlur} />
+
+        {formik.touched.taskDescription && formik.errors.taskDescription ? (<div>{formik.errors.taskDescription}</div>) : null}
+
         <IconButton aria-label="add task" sx={{ width: '10%' }} type='submit' >
           <AddBoxOutlinedIcon />
         </IconButton>
